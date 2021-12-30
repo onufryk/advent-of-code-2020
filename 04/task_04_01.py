e74dc26a6c952b958b8b7595c0563a832d01558e
Roman Onufryk <roman.onufryk@doordash.com>
2021-12-29 16:00:44 -0800

    Fixes for 04 and 05

--- 04/task_04_01.py
+++ 04/task_04_01.py
@@ -1,17 +1,29 @@
+from copy import deepcopy
 required_fields = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'}
 
+valid = 0
 passports = []
-current_passport = set()
-valid_count = 0
+
 with open('input.txt', 'r') as data:
-  for line in data:
-    if line == '\n':
+  current_passport = set()
+  for input_line in data:
+    input_line = input_line.strip()
+    if input_line == '':
       passports.append(current_passport)
-      if len(required_fields - current_passport) == 0:
-        valid_count += 1
+      current_passport = set()
+    else:
+      current_passport |= {item.split(':')[0] for item in input_line.split(' ')}
 
-      current_passport.clear()
-      continue
-    current_passport |= {item.split(':')[0] for item in line.split(' ')}
+  if len(current_passport) > 0:
+    passports.append(current_passport)
+    current_passport = set()
 
-print(valid_count)
+for passport in passports:
+  print(passport)
+  if len(required_fields - passport) == 0:
+    valid += 1
+
+  print(required_fields - passport)
+  print(passport - required_fields)
+
+print(valid)
--- 05/task_05_02.py
+++ 05/task_05_02.py
@@ -1,11 +1,12 @@
 seats = []
 with open('input.txt', 'r') as data:
-  max_seat_id = 0
   for ticket in data:
+    ticket = ticket.strip()
     row_min = 0
     row_max = 127
     seat_min = 0
     seat_max = 7
+    seat_id = None
     for c in ticket:
       if c == 'F':
         row_max = row_max - (row_max - row_min) // 2 - 1
@@ -16,11 +17,10 @@ with open('input.txt', 'r') as data:
       elif c == 'R':
         seat_min = seat_min + (seat_max - seat_min) // 2 + 1
       seat_id = row_min * 8 + seat_min
-      seats.append(seat_id)
-      if seat_id > max_seat_id:
-        max_seat_id = seat_id
 
-  sorted_seats = sorted(seats)
-  for i in range(1, len(sorted_seats)):
-    if sorted_seats[i] - sorted_seats[i - 1] == 2:
-      print(sorted_seats[i - 1], sorted_seats[i])
+    seats.append(seat_id)
+
+seats = sorted(seats)
+for i in range(1, len(seats)):
+  if seats[i] - seats[i - 1] == 2:
+    print(seats[i] - 1)
